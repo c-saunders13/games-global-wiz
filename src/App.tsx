efa40eee3cf677c2a65755c979ef81b609971828
saunders-c <98392650+saunders-c@users.noreply.github.com>
2025-04-03 22:31:34 +0200

    Added products page with responsiveness and mock css graphics

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FaChevronRight } from "react-icons/fa6";
+import "./App.scss";
+import {} from "react-icons";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="page-wrapper">
+        <div className="header">
+          <h1>Market leader in product</h1>
+        </div>
+        <div className="card-wrapper">
+          <div className="card">
+            <div className="card-header">
+              <p>
+                G2 Grid for Cloud-Native Application Protection Platforms
+                (CNAPP)
+              </p>
+            </div>
+            <div className="card-graphic">
+              <div className="card-graphic-wrapper">
+                <div id="grid-graphic">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+            <div className="card-footer">
+              G2 CNAPP Report Spring 2024: Still the #1 CNAPP platform.
+            </div>
+            <div className="card-actions">
+              <button className="card-button">
+                Get Report <FaChevronRight />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-header">
+              <p>The forester wave</p>
+              <p>Cloud Workload Security, Q1 2024</p>
+            </div>
+            <div className="card-graphic">
+              <div className="card-graphic-wrapper">
+                <div id="quarter-graphic">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+            <div className="card-footer">
+              Wiz recognized with top score for the current offering category in
+              The Forrester Wave™: Cloud Workload Security, Q1, 2024.
+            </div>
+            <div className="card-actions">
+              <button className="card-button">
+                Get Report <FaChevronRight />
+              </button>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-header">
+              <p>Frost Radar™</p>
+              <p>Cloud Security Posture Management, 2024</p>
+            </div>
+            <div className="card-graphic">
+              <div className="card-graphic-wrapper">
+                <div id="radar-graphic">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+            <div className="card-footer">
+              Frost & Sullivan recognizes Wiz as Cloud Security Posture
+              Management leader.
+            </div>
+            <div className="card-actions">
+              <button className="card-button">
+                Get Report <FaChevronRight />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
